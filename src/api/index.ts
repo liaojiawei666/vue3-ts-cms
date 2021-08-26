f4b961a8c3b3703aba4c0afa4658e43c3582326f
Administrator <2547456640@qq.com>
2021-08-27 01:03:22 +0800

    封装拦截器，拦截器添加加载动画

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,6 +4,7 @@ import { BASE_URL, TIME_OUT } from "@/api/request/config";
 const cmsRequest = new CMSRequest({
   baseURL: BASE_URL,
   timeout: TIME_OUT,
+  showLoading: false,
   interceptors: {
     requestInterceptor: (config) => {
       console.log("cms请求成功的拦截");
@@ -12,6 +13,14 @@ const cmsRequest = new CMSRequest({
     requestInterceptorCatch: (error) => {
       console.log("cms请求失败的拦截");
       console.log(error);
+    },
+    responseInterceptor: (res) => {
+      console.log("cms响应成功的拦截");
+      return res;
+    },
+    responseInterceptorCatch: (err) => {
+      console.log("cms响应失败的拦截");
+      return err;
     },
   },
 });
--- src/main.ts
+++ src/main.ts
@@ -9,5 +9,24 @@ import cmsRequest from "@/api";
 cmsRequest.request({
   url: "/home/multidata",
   method: "GET",
+  showLoading: true,
+  interceptors: {
+    requestInterceptor(config) {
+      console.log("单次请求成功的拦截");
+      return config;
+    },
+    requestInterceptorCatch: (error) => {
+      console.log("单次请求失败的拦截");
+      console.log(error);
+    },
+    responseInterceptor: (res) => {
+      console.log("单次响应成功的拦截");
+      return res;
+    },
+    responseInterceptorCatch: (err) => {
+      console.log("单次响应失败的拦截");
+      return err;
+    },
+  },
 });
 createApp(App).use(store).use(router).use(ElementPlus).mount("#app");
